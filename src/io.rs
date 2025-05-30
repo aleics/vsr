1aba3be3b181e6321080b346ab764d0c0b6095a6
aleics <aleics.casanovas@gmail.com>
2025-05-30 21:10:38 +0200

    Use `mio` instead of `polling` for IO implementation
    * Allow to read and write buffers directly from the outside (to allow
    message framing)
    * Fix accepting multiple connections at once whenever multiple streams
    are waited to be accepted.

--- src/io.rs
+++ src/io.rs
@@ -1,100 +1,88 @@
-use std::net::{TcpListener, TcpStream};
-use std::num::NonZeroUsize;
 use std::{io::Read, net::SocketAddr};
 use std::{io::Write, time::Duration};
 
-use bincode::config::Configuration;
-use polling::{Event, Events, Poller};
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
 use thiserror::Error;
 
 use crate::MESSAGE_SIZE_MAX;
-use crate::network::Message;
 
-const SERVER: usize = 0;
-const EVENTS_CAPACITY: NonZeroUsize = NonZeroUsize::new(128).unwrap();
+const SERVER: Token = Token(0);
+const EVENTS_CAPACITY: usize = 128;
 
+#[derive(Debug)]
 pub enum Completion {
     Accept,
     Recv { connection: usize },
+    Write { connection: usize },
 }
 
-pub enum RecvBody {
-    Message { message: Box<Message> },
-    SocketOccupied,
-    Close,
+pub struct AcceptedConnection {
+    pub(crate) socket: TcpStream,
+    pub(crate) connection_id: usize,
 }
 
 pub trait IO {
     fn open_tcp(&self, addr: SocketAddr) -> Result<TcpListener, IOError>;
 
-    fn connect(&mut self, addr: SocketAddr, connection_id: usize) -> Result<TcpStream, IOError>;
+    fn connect(
+        &mut self,
+        addr: SocketAddr,
+        connection_id: usize,
+    ) -> Result<Option<TcpStream>, IOError>;
 
     fn accept(
         &mut self,
         socket: &TcpListener,
         connection_id: usize,
-    ) -> Result<Option<TcpStream>, IOError>;
+    ) -> Result<Vec<AcceptedConnection>, IOError>;
 
-    fn recv(&self, socket: &mut TcpStream, connection_id: usize) -> Result<RecvBody, IOError>;
+    fn recv(&self, socket: &mut TcpStream, buffer: &mut Vec<u8>) -> Result<bool, IOError>;
 
-    fn send(&self, message: Message, socket: &mut TcpStream) -> Result<(), IOError>;
+    fn send(&self, socket: &mut TcpStream, connection_id: usize) -> Result<(), IOError>;
+
+    fn write(&self, socket: &mut TcpStream, buffer: &[u8]) -> Result<Option<usize>, IOError>;
 
     fn run(&mut self, duration: Duration) -> Result<Vec<Completion>, IOError>;
 }
 
 pub struct PollIO {
-    poll: Poller,
+    poll: Poll,
     events: Events,
-    config: Configuration,
 }
 
 impl PollIO {
     pub fn new() -> Result<Self, IOError> {
         Ok(Self {
-            poll: Poller::new()?,
+            poll: Poll::new()?,
             events: Events::with_capacity(EVENTS_CAPACITY),
-            config: bincode::config::standard(),
         })
     }
 }
 
 impl IO for PollIO {
     fn open_tcp(&self, addr: SocketAddr) -> Result<TcpListener, IOError> {
-        let listener = TcpListener::bind(addr)?;
-        listener.set_nonblocking(true)?;
+        let mut listener = TcpListener::bind(addr)?;
 
-        unsafe {
-            self.poll.add(&listener, Event::readable(SERVER))?;
-        }
+        self.poll
+            .registry()
+            .register(&mut listener, SERVER, Interest::READABLE)?;
 
         Ok(listener)
     }
 
-    fn connect(&mut self, addr: SocketAddr, connection_id: usize) -> Result<TcpStream, IOError> {
-        match TcpStream::connect(addr) {
-            Ok(stream) => {
-                unsafe {
-                    self.poll.add(&stream, Event::readable(connection_id))?;
-                }
-
-                Ok(stream)
-            }
-            Err(e) => Err(e)?,
-        }
-    }
-
-    fn accept(
+    fn connect(
         &mut self,
-        socket: &TcpListener,
+        addr: SocketAddr,
         connection_id: usize,
     ) -> Result<Option<TcpStream>, IOError> {
-        assert!(connection_id != SERVER);
-
-        match socket.accept() {
-            Ok((stream, _)) => {
-                unsafe {
-                    self.poll.add(&stream, Event::all(connection_id))?;
-                }
+        match TcpStream::connect(addr) {
+            Ok(mut stream) => {
+                self.poll.registry().register(
+                    &mut stream,
+                    Token(connection_id),
+                    Interest::WRITABLE,
+                )?;
 
                 Ok(Some(stream))
             }
@@ -103,55 +91,111 @@ impl IO for PollIO {
         }
     }
 
-    fn recv(&self, socket: &mut TcpStream, connection_id: usize) -> Result<RecvBody, IOError> {
-        let mut buf = [0; MESSAGE_SIZE_MAX];
+    fn accept(
+        &mut self,
+        socket: &TcpListener,
+        connection_id: usize,
+    ) -> Result<Vec<AcceptedConnection>, IOError> {
+        assert!(connection_id != SERVER.0);
 
-        let n = match socket.read(&mut buf) {
-            Ok(n) => Ok(n),
-            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                return Ok(RecvBody::SocketOccupied);
+        let mut accepted = Vec::new();
+        let mut next_connection_id = connection_id;
+
+        loop {
+            // An accept event might include multiple connections waiting to
+            // be accepted. Thus, we'll accept everything from the socket.
+            // A `WouldBlock` error would signal that there are no more
+            // connections to accept.
+            match socket.accept() {
+                Ok((mut stream, _)) => {
+                    self.poll.registry().register(
+                        &mut stream,
+                        Token(next_connection_id),
+                        Interest::READABLE,
+                    )?;
+
+                    accepted.push(AcceptedConnection {
+                        socket: stream,
+                        connection_id: next_connection_id,
+                    });
+                    next_connection_id += 1;
+                }
+                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                Err(e) => Err(e)?,
             }
-            Err(e) => Err(e),
-        }?;
-
-        // Keep interest into read events from this socket
-        self.poll.modify(&socket, Event::readable(connection_id))?;
-
-        if n == 0 {
-            return Ok(RecvBody::Close);
         }
 
-        let (message, _) = bincode::decode_from_slice(&buf, self.config)?;
-        Ok(RecvBody::Message { message })
+        Ok(accepted)
     }
 
-    fn send(&self, message: Message, socket: &mut TcpStream) -> Result<(), IOError> {
+    fn recv(&self, socket: &mut TcpStream, buffer: &mut Vec<u8>) -> Result<bool, IOError> {
         let mut buf = [0; MESSAGE_SIZE_MAX];
-        bincode::encode_into_slice(message, &mut buf, self.config)?;
 
-        socket.write_all(&buf)?;
+        loop {
+            let n = match socket.read(&mut buf) {
+                Ok(0) => return Ok(true),
+                Ok(n) => Ok(n),
+                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    break;
+                }
+                Err(e) => Err(e),
+            }?;
+
+            buffer.extend_from_slice(&buf[..n]);
+        }
+
+        Ok(false)
+    }
+
+    fn send(&self, socket: &mut TcpStream, connection_id: usize) -> Result<(), IOError> {
+        self.poll.registry().reregister(
+            socket,
+            Token(connection_id),
+            Interest::READABLE.add(Interest::WRITABLE),
+        )?;
 
         Ok(())
     }
 
+    fn write(&self, socket: &mut TcpStream, buf: &[u8]) -> Result<Option<usize>, IOError> {
+        let written = match socket.write(buf) {
+            Ok(0) => Err(std::io::ErrorKind::WriteZero.into()),
+            Ok(n) => Ok(n),
+            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => return Ok(None),
+            Err(e) => Err(e),
+        }?;
+
+        Ok(Some(written))
+    }
+
     fn run(&mut self, duration: Duration) -> Result<Vec<Completion>, IOError> {
         self.events.clear();
-        self.poll.wait(&mut self.events, Some(duration))?;
+        self.poll.poll(&mut self.events, Some(duration))?;
 
-        let mut completions = Vec::with_capacity(EVENTS_CAPACITY.get());
+        let mut completions = Vec::with_capacity(EVENTS_CAPACITY);
 
         for event in self.events.iter() {
-            if event.is_err().unwrap_or_default() {
-                // TODO: handle connection failed
-                continue;
-            }
+            match event.token() {
+                SERVER => {
+                    completions.push(Completion::Accept);
+                }
+                token => {
+                    // The event notifies that the socket can be read on (incoming messages).
+                    if event.is_readable() {
+                        completions.push(Completion::Recv {
+                            connection: token.0,
+                        })
+                    }
 
-            let completion = match event.key {
-                SERVER => Completion::Accept,
-                token => Completion::Recv { connection: token },
+                    // The event notifies that the socket can be written on, thus, schedule a write of any pending
+                    // events in the connection buffer.
+                    if event.is_writable() {
+                        completions.push(Completion::Write {
+                            connection: token.0,
+                        })
+                    }
+                }
             };
-
-            completions.push(completion);
         }
 
         Ok(completions)
